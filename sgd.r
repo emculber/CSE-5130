9639f3f57d4bec318a2be718394fdb99f633b9d8
Erik <eculberson12@gmail.com>
2018-02-13 21:27:55 -0500

    Added problem 4

--- sgd.r
+++ sgd.r
@@ -4,23 +4,23 @@ count = 0
 w0 = 0
 w1 = 0
 learning_rate = 0.1
-while(count < 10) {
+while(count < 20) {
+  print(paste0("n & = ", count + 1))
   # w0 := w0 - alpha * 2/N * Sum(w0 + w1*x - y) 
   # w1 := w1 - alpha * 2/N * Sum(w0 + w1*x - y) * x
-  #w0temp <- w0 - 0.1 * 2/3 * sum(w0 + w1*S[1,] - S[2,])
-  #w1temp <- w1 - 0.1 * 2/3 * sum((w0 + w1*S[1,] - S[2,])*S[1,]) 
-  #print(paste0(w0temp, " = ", w0, " - ", 0.1, " * 2/3 * ", sum(w0 + w1*S[1,] - S[2,])))
-  #print(paste0(w1temp, " = ", w1, " - ", 0.1, " * 2/3 * ", sum((w0 + w1*S[1,] - S[2,])*S[1,])))
-  #w0 = w0temp
-  #w1 = w1temp
-  print(paste0("n = ", count + 1))
-  for(s in S) {
-    w0temp <- w0 - learning_rate * (w0 + w1*s[1] - s[2])
-    w1temp <- w1 - learning_rate * (w0 + w1*s[1] - s[2]) * s[1]
-    print(paste0("w_0 & = ", w0, " - 0.1 * ", "(",w0, "+",w1,"*",s[1],"-",s[2],") * 1 = ", w0temp, "\\"))
-    print(paste0("w_1 & = ", w1, " - 0.1 * ", "(",w0, "+",w1,"*",s[1],"-",s[2],") * ", s[1], " = ", w1temp, "\\"))
-    w0 <- w0temp
-    w1 <- w1temp
-  }
+  w0temp <- w0 - 0.1 * 2/3 * sum(w0 + w1*S[1,] - S[2,])
+  w1temp <- w1 - 0.1 * 2/3 * sum((w0 + w1*S[1,] - S[2,])*S[1,]) 
+  print(paste0("w_0 & = ", w0, " - 0.1 * 2/3 * ", sum(w0 + w1*S[1,] - S[2,]), " = ", w0temp, "\\"))
+  print(paste0("w_1 & = ", w1, " - 0.1 * 2/3 * ", sum((w0 + w1*S[1,] - S[2,])*S[1,]), " = ", w1temp, "\\"))
+  w0 = w0temp
+  w1 = w1temp
+  #for(s in S) {
+  #  w0temp <- w0 - learning_rate * (w0 + w1*s[1] - s[2])
+  #  w1temp <- w1 - learning_rate * (w0 + w1*s[1] - s[2]) * s[1]
+  #  print(paste0("w_0 & = ", w0, " - 0.1 * ", "(",w0, "+",w1,"*",s[1],"-",s[2],") * 1 = ", w0temp, "\\"))
+  #  print(paste0("w_1 & = ", w1, " - 0.1 * ", "(",w0, "+",w1,"*",s[1],"-",s[2],") * ", s[1], " = ", w1temp, "\\"))
+  #  w0 <- w0temp
+  #  w1 <- w1temp
+  #}
   count=count+1
 }
